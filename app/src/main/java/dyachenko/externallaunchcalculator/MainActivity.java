b1421906ad819d41669ce6be969a023f36ab8dfe
YuriDyachenko <yuri.dyachenko@outlook.com>
2021-05-11 00:00:30 +0300

    Только основные файлы с изменениями для ознакомления

---
+++ app/src/main/java/dyachenko/externallaunchcalculator/MainActivity.java
@@ -0,0 +1,24 @@
+package dyachenko.externallaunchcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.extra_calculator).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("dyachenko.androidbeginnercalculator");
+                startActivity(intent);
+            }
+        });
+    }
+}
